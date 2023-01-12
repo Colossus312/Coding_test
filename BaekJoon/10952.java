6d23dc2e45e2a947cfc7309f72a5ea5890206f4e
Colossus312 <alehdla@gmail.com>
2023-01-12 21:13:11 +0900

    update

---
+++ BaekJoon/10952.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String buf = "";
+        while ((buf = br.readLine()) != null) {
+            StringTokenizer A = new StringTokenizer(buf," ");
+            int B = Integer.parseInt(A.nextToken());
+            int C = Integer.parseInt(A.nextToken());
+            if(B == C && B == 0){
+
+            }else{
+                System.out.println(B+C);
+            }
+
+        }
+
+        br.close();
+
+    }
+
+}
