56b89434e77184d9003c07bf27e5e909d74c5d14
Colossus312 <alehdla@gmail.com>
2023-01-22 12:33:50 +0900

    update

---
+++ Programmers/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+
+        int answer = 0;
+
+        String number = String.valueOf(n);
+
+        for(int i = 0; i < number.length(); i++){
+            answer += Integer.parseInt(number.substring(i,i+1));
+        }
+
+        return answer;
+    }
+}
