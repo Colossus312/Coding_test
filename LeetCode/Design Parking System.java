7f1ab49173c05858497b7fea36f63ba0095cde52
Colossus312 <alehdla@gmail.com>
2023-04-24 20:59:16 +0900

    update

---
+++ LeetCode/Design Parking System.java
@@ -0,0 +1,21 @@
+class ParkingSystem {
+
+    public ParkingSystem(int big, int medium, int small) {
+        count = new int[] {0, big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        if (count[carType] == 0) {
+            return false;
+        }
+        count[carType]--;
+        return true;
+    }
+    private int[] count;
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
