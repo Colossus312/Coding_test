796264dd469c09dd235bba8f7ac44186401e58c0
Colossus312 <alehdla@gmail.com>
2023-05-23 20:02:26 +0900

    update

---
+++ Programmers/짝수 홀수 개수.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        return new int[]{(int)Arrays.stream(num_list).filter(n -> n % 2 == 0).count(), (int)Arrays.stream(num_list).filter(n -> n % 2 != 0).count()};
+    }
+}
