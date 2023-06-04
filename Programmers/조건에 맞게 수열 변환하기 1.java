02f248dc11e322edba4786c32e1b4a4ab51b5619
Colossus312 <alehdla@gmail.com>
2023-06-04 11:47:11 +0900

    update

---
+++ Programmers/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr) {
+        return Arrays.stream(arr)
+                .map(n -> n >= 50 && n % 2 == 0 ? n / 2 : n < 50 && n % 2 != 0 ? n * 2 : n)
+                .toArray();
+    }
+}
