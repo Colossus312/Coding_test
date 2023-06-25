232f58ad7e50d91aa7233d35da9a341414559a01
Colossus312 <alehdla@gmail.com>
2023-06-25 23:57:22 +0900

    update

--- Programmers/문자열 바꿔서 찾기.java
+++ Programmers/문자열 바꿔서 찾기.java
@@ -1,1 +1,5 @@
-
+class Solution {
+    public int solution(String myString, String pat) {
+        return myString.replaceAll("A","b").replaceAll("B","a").toUpperCase().indexOf(pat) == -1 ? 0 : 1;
+    }
+}
