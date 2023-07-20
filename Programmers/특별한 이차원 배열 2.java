4f0135099a4874137b63ed3874446ed79cbccf28
Colossus312 <alehdla@gmail.com>
2023-07-20 22:49:23 +0900

    update

---
+++ Programmers/특별한 이차원 배열 2.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[][] arr) {
+        return IntStream.range(0, arr.length).allMatch(i -> IntStream.range(0, i + 1).allMatch(j -> arr[i][j] == arr[j][i])) ? 1 : 0;
+    }
+}
