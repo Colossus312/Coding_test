eddf44a6816c576de7bcb4c725c9e221d8fb0bb3
Colossus312 <alehdla@gmail.com>
2023-08-06 12:44:28 +0900

    update

---
+++ Programmers/문자열 돌리기.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (char c : new Scanner(System.in).next().toCharArray()) System.out.println(c);
+    }
+}
