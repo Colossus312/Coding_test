44d9c76fe20b2cc92ee1436397539a5fdf846ac2
Colossus312 <alehdla@gmail.com>
2023-09-03 23:11:43 +0900

    update

--- Programmers/1로 만들기.java
+++ Programmers/1로 만들기.java
@@ -1,1 +1,7 @@
+import java.util.Arrays;
 
+class Solution {
+    public int solution(int[] num_list) {
+        return Arrays.stream(num_list).map(i -> {int count = 0; while (i != 1) {i = (i % 2 == 0) ? i / 2 : (i - 1) / 2; count++;} return count;}).sum();
+    }
+}
