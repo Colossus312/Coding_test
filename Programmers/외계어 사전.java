9584094cb677b4d70d0993d54a804056fa8e7ae6
Colossus312 <alehdla@gmail.com>
2023-10-09 21:35:39 +0900

    update

--- Programmers/외계어 사전.java
+++ Programmers/외계어 사전.java
@@ -1,1 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        return Arrays.stream(dic).anyMatch(dicWord -> Arrays.stream(dicWord.split("")).sorted().collect(Collectors.joining()).equals(Arrays.stream(spell).sorted().collect(Collectors.joining())) && dicWord.length() == spell.length) ? 1 : 2;
+    }
+}
