028db2d4261f3db767deeca12ba694a9048e0f85
Colossus312 <alehdla@gmail.com>
2024-01-20 18:16:13 +0900

    update

---
+++ BaekJoon/10988.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        br.close();
+
+        boolean isPalindrome = isPalindrome(word);
+        System.out.println(isPalindrome ? 1 : 0);
+    }
+
+    private static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
