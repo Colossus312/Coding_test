089d435c0533750a15cff8b533b1d14762b3915b
Colossus312 <alehdla@gmail.com>
2024-01-30 01:48:45 +0900

    update

---
+++ BaekJoon/1193.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine()); // Read the input
+        int diagonal = 1, count = 0;
+
+        while (count < X) {
+            count += diagonal;
+            diagonal++;
+        }
+
+        diagonal--;
+        count -= diagonal;
+
+        int numerator, denominator;
+        if (diagonal % 2 == 0) {
+            numerator = X - count;
+            denominator = diagonal - numerator + 1;
+        } else {
+            denominator = X - count;
+            numerator = diagonal - denominator + 1;
+        }
+
+        System.out.println(numerator + "/" + denominator);
+    }
+}
