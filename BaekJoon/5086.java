f2c029e9342c698daefbc3d286abb9e8555d555a
Colossus312 <alehdla@gmail.com>
2024-02-01 23:33:49 +0900

    update

---
+++ BaekJoon/5086.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while (!(line = reader.readLine()).equals("0 0")) {
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[0]), b = Integer.parseInt(parts[1]);
+
+            if (b % a == 0) {
+                System.out.println("factor");
+            } else if (a % b == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
