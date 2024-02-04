82c851ac937521e17daa546a5adebe2f3cff8384
Colossus312 <alehdla@gmail.com>
2024-02-05 00:06:25 +0900

    update

---
+++ BaekJoon/2581.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        boolean foundPrime = false;
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (i < min) {
+                    min = i;
+                }
+                foundPrime = true;
+            }
+        }
+
+        if (foundPrime) {
+            System.out.println(sum);
+            System.out.println(min);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
