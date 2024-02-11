3f37d03b34190742d09f71edb313b4b1b90e0611
Colossus312 <alehdla@gmail.com>
2024-02-11 22:21:10 +0900

    update

---
+++ BaekJoon/9063.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int minX = 10001, maxX = -10001, minY = 10001, maxY = -10001;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+
+        int area = (maxX - minX) * (maxY - minY);
+
+        System.out.println(area);
+    }
+}
