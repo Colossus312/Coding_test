ca0ec5268ee40fc15b539760c91e58b7c5b32a61
Colossus312 <alehdla@gmail.com>
2024-02-23 19:32:28 +0900

    update

---
+++ BaekJoon/24266.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        long executionCount = (long) n * n * n;
+
+        System.out.println(executionCount);
+        System.out.println(3);
+    }
+}
