fd7124409161cd56d7acb4defc561ab048e15c7e
Colossus312 <alehdla@gmail.com>
2024-03-15 19:18:25 +0900

    update

--- BaekJoon/14425.java
+++ BaekJoon/14425.java
@@ -1,1 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputLine = br.readLine().split(" ");
+        int N = Integer.parseInt(inputLine[0]);
+        int M = Integer.parseInt(inputLine[1]);
+
+        HashSet<String> setS = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            setS.add(br.readLine());
+        }
+
+        int count = 0; // To count how many of M strings are in set S
+
+        for (int i = 0; i < M; i++) {
+            if (setS.contains(br.readLine())) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
