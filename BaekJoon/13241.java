7a7ca4255c024ba21ec40373000af865ce338f83
Colossus312 <alehdla@gmail.com>
2024-03-23 20:14:53 +0900

    update

---
+++ BaekJoon/13241.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+
+        System.out.println(lcm(a, b));
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
