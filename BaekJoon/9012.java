5fb10f819ea9c7c86f0cf4cc4b4cbd714ab1983d
Colossus312 <alehdla@gmail.com>
2024-04-02 21:37:59 +0900

    update

---
+++ BaekJoon/9012.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            System.out.println(isVPS(br.readLine()) ? "YES" : "NO");
+        }
+    }
+
+    private static boolean isVPS(String s) {
+        int balance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance < 0) {
+                return false;
+            }
+        }
+
+        return balance == 0;
+    }
+}
