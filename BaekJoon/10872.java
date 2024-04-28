50798d6c9f47e8f04b9e9021b6a2b75ddc9190e0
Colossus312 <alehdla@gmail.com>
2024-04-28 16:27:42 +0900

    update

--- BaekJoon/10872.java
+++ BaekJoon/10872.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        scanner.close();
+
+        int factorial = 1;
+        for (int i = 2; i <= N; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+    }
+}
